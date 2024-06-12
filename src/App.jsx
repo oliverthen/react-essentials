51497dcd28bc3bc298835597420c74653530acf7
Oliver Then <oli171dr@gmail.com>
2024-06-12 19:05:23 -0400

    First Component Made

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-function App() {
-  return (
-    <div>
-      <header>
+function Header() {
+	return (
+		<header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
-      </header>
+      	</header>
+	);
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>

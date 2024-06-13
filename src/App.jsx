bdcff427efc5238a71e723d107d993a232db5090
Oliver Then <oli171dr@gmail.com>
2024-06-12 20:33:56 -0400

    Initial Props Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+	const description = reactDescriptions[getRandomInt(2)]
+
 	return (
 		<header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       	</header>
+	);
+}
+
+function CoreConcept({image, title, description}) {
+	return (
+		<li>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</li>
 	);
 }
 
@@ -16,7 +38,16 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+		<section id="core-concepts">
+			<h2>Core Concepts</h2>
+			<ul>
+				<CoreConcept {...CORE_CONCEPTS[0]}/>
+				<CoreConcept {...CORE_CONCEPTS[1]} />
+				<CoreConcept {...CORE_CONCEPTS[2]} />
+				<CoreConcept {...CORE_CONCEPTS[3]} />
+				
+			</ul>
+		</section>
       </main>
     </div>
   );

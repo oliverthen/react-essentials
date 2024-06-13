13c26c1c9c4ef789ea19feaec38f836b290f6117
Oliver Then <oli171dr@gmail.com>
2024-06-13 19:57:33 -0400

    Event Handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import TabButton from './components/TabButton';
 
 
 function App() {
-	function handleSelect() {
-		console.log('Hello world - selected!');
+	function handleSelect(selectedButton) {
+		console.log(selectedButton);
 	}
 
   return (
@@ -26,10 +26,10 @@ function App() {
 		<section id="examples">
 			<h2>Examples</h2>
 			<menu>
-				<TabButton onSelect={handleSelect}>Components</TabButton>
-				<TabButton onSelect={handleSelect}>JSX</TabButton>
-				<TabButton onSelect={handleSelect}>Props</TabButton>
-				<TabButton onSelect={handleSelect}>State</TabButton>
+				<TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+				<TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+				<TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+				<TabButton onSelect={() => handleSelect('state')}>State</TabButton>
 			</menu>
 
 		</section>

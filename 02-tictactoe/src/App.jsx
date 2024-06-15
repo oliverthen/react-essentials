cb2d539d1fad2d1313f7a29ff45106c3e071530f
Oliver Then <oli171dr@gmail.com>
2024-06-15 13:55:43 -0400

    Work on Tic Tac Toe Player Component

--- 02-tictactoe/src/App.jsx
+++ 02-tictactoe/src/App.jsx
@@ -1,10 +1,23 @@
+import Player from "./components/Player";
 
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+				<ol id="players">
+					<Player name="Player 1" symbol="X"/>
+					<Player name="Player 2" symbol="O"/>
+				
+				</ol>
+
+				GAME BOARD
+			</div>
+
+			LOG
+	  </main>
+  );
 }
 
 export default App
---
+++ 02-tictactoe/src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Player({name, symbol}) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	function handleEditClick() {
+		setIsEditing(() => !isEditing);
+	}
+
+	let playerName =  <span className="player-name">{name}</span>;
+	if (isEditing) {
+		playerName = <input type="text" required value={name}/>
+	}
+	
+
+	return (
+	<li>
+		<span className="player">
+			{playerName}
+			<span className="player-symbol">{symbol}</span>
+		</span>
+		<button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+	</li>);
+}

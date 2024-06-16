5a4b161bbfa8472e1877f00f2949d5bf17054b99
Oliver Then <oli171dr@gmail.com>
2024-06-16 09:50:26 -0400

    Implement Log Feature and get rid of unnecesarry state

--- 02-tictactoe/src/App.jsx
+++ 02-tictactoe/src/App.jsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+
+function deriveActivePlayer(gameTurns) {
+	let currentPlayer = 'X';
+
+	if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+		currentPlayer = 'O';
+	}
+
+	return currentPlayer;
+}
+
 
 function App() {
-  
+	const [gameTurns, setGameTurns] = useState([]);
+	// const [activePlayer, setActivePlayer] = useState('X');
 
+	const activePlayer = deriveActivePlayer(gameTurns);
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		// setActivePlayer((curActivePlayer) => curActivePlayer === 'X'? 'O' : 'X');
+		setGameTurns(prevTurns => {
+			const currentPlayer = deriveActivePlayer(prevTurns);
+
+			const updatedTurns = [
+				{square: { row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns
+			];
+
+			return updatedTurns;
+		});
+
+	}
   return (
     <main>
       <div id="game-container">
-				<ol id="players">
-					<Player name="Player 1" symbol="X"/>
-					<Player name="Player 2" symbol="O"/>
-				
+				<ol id="players" className="highlight-player">
+					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>				
 				</ol>
-
-				GAME BOARD
+				<GameBoard onSelectSquare = {handleSelectSquare} turns={gameTurns}/>
 			</div>
 
-			LOG
+			<Log turns={gameTurns} />
 	  </main>
   );
 }
--- 02-tictactoe/src/components/Player.jsx
+++ 02-tictactoe/src/components/Player.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol, isActive }) {
+	const [playerName, setPlayerName ] = useState(initialName);
 	const [isEditing, setIsEditing] = useState(false);
 
 	function handleEditClick() {
-		setIsEditing(() => !isEditing);
+		setIsEditing(editing => !editing);
 	}
 
-	let playerName =  <span className="player-name">{name}</span>;
+	function handleChange(event) {
+		setPlayerName(event.target.value);
+	}
+
+	let editablePlayerName =  <span className="player-name">{playerName}</span>;
+
 	if (isEditing) {
-		playerName = <input type="text" required value={name}/>
+		editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
 	}
 	
 
 	return (
-	<li>
+	<li className={isActive ? 'active': undefined}>
 		<span className="player">
-			{playerName}
+			{editablePlayerName}
 			<span className="player-symbol">{symbol}</span>
 		</span>
 		<button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>

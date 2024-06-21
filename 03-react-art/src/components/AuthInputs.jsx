63503cfda8df7095f1bfe9154584806d0d84a790
Oliver Then <oli171dr@gmail.com>
2024-06-21 12:33:15 -0400

    Working with Tailwind CSS

--- 03-react-art/src/components/AuthInputs.jsx
+++ 03-react-art/src/components/AuthInputs.jsx
@@ -1,19 +1,7 @@
 import { useState } from 'react';
-import { styled} from 'styled-components';
 
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControlContainer = styled.div`
-	display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-
-
-
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -36,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
 						label="Email"
             type="email"
@@ -52,9 +40,9 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>

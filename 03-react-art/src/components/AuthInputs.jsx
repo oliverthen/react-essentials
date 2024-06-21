938b98c3574c32c69cd679c064035a7e3166a231
Oliver Then <oli171dr@gmail.com>
2024-06-20 20:58:16 -0400

    More work with Styled Components

--- 03-react-art/src/components/AuthInputs.jsx
+++ 03-react-art/src/components/AuthInputs.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { styled} from 'styled-components';
 
+import Button from './Button.jsx';
+import Input from './Input.jsx';
+
 const ControlContainer = styled.div`
 	display: flex;
   flex-direction: column;
@@ -8,26 +11,9 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `
 
-const Label = styled.label`
-	display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
 
-const Input = styled.input`
-	width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : '#transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+
+
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -52,30 +38,26 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
           <Input
+						label="Email"
             type="email"
 						$invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
+						label="Password"
             type="password"
             $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );

75321ee475d6b4fba447134c1373b8c4c04ca3ca
Oliver Then <oli171dr@gmail.com>
2024-06-27 07:37:55 -0400

    Practice with useReducer hook

--- 06-advanced-state-management/src/components/CartModal.jsx
+++ 06-advanced-state-management/src/components/CartModal.jsx
@@ -2,11 +2,14 @@ import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart';
 
+
 const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
+  { title, actions },
   ref
 ) {
   const dialog = useRef();
+
+	
 
   useImperativeHandle(ref, () => {
     return {
@@ -19,7 +22,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>

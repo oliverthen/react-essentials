6675019f81583adead45459dc6e2a74f7e74383b
Oliver Then <oli171dr@gmail.com>
2024-07-29 18:44:48 -0400

    Handling Form Submission and using state for user input

--- 13-forms/src/components/Login.jsx
+++ 13-forms/src/components/Login.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
+
 export default function Login() {
+	// const [enteredEmail, setEnteredEmail] = useState('');
+	// const [enteredPassword, setEnteredPassword] = useState('');
+	const [enteredValues, setEnteredValues] = useState({
+		email: '',
+		password: ''
+	});
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log(enteredValues);
+
+	}
+
+	function handleInputChange(identifier, value) {
+		// setEnteredValues(event.target.value);
+		setEnteredValues(prevValues => ({
+			...prevValues,
+			[identifier]: value
+		}));
+	}
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => handleInputChange("email", event.target.value)} value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => handleInputChange("password", event.target.value)} value={enteredValues.password}/>
         </div>
       </div>
 

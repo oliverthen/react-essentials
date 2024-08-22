8600d7397706d91739e7706fb9d7fd0241114162
Oliver Then <oli171dr@gmail.com>
2024-08-22 19:39:53 -0400

    attaching payload to actions and working with multiple state properties

--- 14-redux/src/components/Counter.js
+++ 14-redux/src/components/Counter.js
@@ -5,23 +5,31 @@ import classes from './Counter.module.css';
 const Counter = () => {
 	const dispatch = useDispatch();
 	const counter = useSelector(state => state.counter);
+	const show = useSelector(state => state.showCounter);
 
 	const incrementHandler = () => {
 		dispatch({ type: 'increment'});
 	};
 
-	const decrementHandler = () => {
-			dispatch({ type: 'decrement'});
+	const increaseHandler = () => {
+		dispatch({ type: 'increase', amount: 5});
 	};
 
-  const toggleCounterHandler = () => {};
+	const decrementHandler = () => {
+		dispatch({ type: 'decrement'});
+	};
+
+  const toggleCounterHandler = () => {
+		dispatch({ type: 'toggle'});
+	};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
 			<div>
 				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={increaseHandler}>Increase by 5</button>
 				<button onClick={decrementHandler}>Decrement</button>
 			</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
